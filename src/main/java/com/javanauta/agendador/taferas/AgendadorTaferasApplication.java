f4f3854831d135010a734d5bc7403b25c887c8a9
jairo <jairoblink@gmail.com>
2026-04-02 09:56:38 -0300

    commit inicial esqueleto

---
+++ src/main/java/com/javanauta/agendador/taferas/AgendadorTaferasApplication.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendador.taferas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendadorTaferasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendadorTaferasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javanauta/agendador/taferas/AgendadorTaferasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javanauta.agendador.taferas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorTaferasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
